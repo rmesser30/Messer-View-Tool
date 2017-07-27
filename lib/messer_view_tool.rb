613579b6dfceaa7c329c69d5e76a4504b634f189
rmesser30 <rmesser30@yahoo.com>
2017-07-27 16:47:06 -0600

    Dev tool initial commit

---
+++ lib/messer_view_tool.rb
@@ -0,0 +1,6 @@
+require "messer_view_tool/version"
+require "messer_view_tool/renderer"
+
+module MesserViewTool
+  # Your code goes here...
+end
---
+++ lib/messer_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MesserViewTool
+  VERSION = "0.1.0"
+end
